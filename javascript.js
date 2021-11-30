5251fe6773423b6f19a22c325179e94cc3464ae7
Rapidity1993 <33816753+Rapidity1993@users.noreply.github.com>
2021-11-30 19:43:06 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,20 @@
+// window.onscroll = function () {
+//     var el = document.getElementsByClassName('header')[0];
+//     var className = 'small';
+//     if (el.classList) {
+//         if (window.scrollY > 10)
+//             el.classList.add(className);
+//         else
+//             el.classList.remove(className);
+//     }
+// };
+// $("#workersButton").click(function () {
+//     $([document.documentElement, document.body]).animate({
+//         scrollTop: $("#workers").offset().top
+//     }, 1000);
+// });
+// $("#connectButton").click(function () {
+//     $([document.documentElement, document.body]).animate({
+//         scrollTop: $("#connect").offset().top
+//     }, 1000);
+// });
