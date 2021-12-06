1c219a510766af3cf3107485012d3f28aeef48d5
Rapidity1993 <33816753+Rapidity1993@users.noreply.github.com>
2021-12-06 13:00:24 +0100

    version 1.1 with slider

--- javascript.js
+++ javascript.js
@@ -1,20 +1,14 @@
-// window.onscroll = function () {
-//     var el = document.getElementsByClassName('header')[0];
-//     var className = 'small';
-//     if (el.classList) {
-//         if (window.scrollY > 10)
-//             el.classList.add(className);
-//         else
-//             el.classList.remove(className);
-//     }
-// };
-// $("#workersButton").click(function () {
-//     $([document.documentElement, document.body]).animate({
-//         scrollTop: $("#workers").offset().top
-//     }, 1000);
-// });
-// $("#connectButton").click(function () {
-//     $([document.documentElement, document.body]).animate({
-//         scrollTop: $("#connect").offset().top
-//     }, 1000);
-// });
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showSlides, 7000); // Change image every 2 seconds
+}
