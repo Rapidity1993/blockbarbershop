98865bc4810ebc5d2c64e7eec868a39b0e2e27a3
Zoltán Horváth <81582945+horvzoltan@users.noreply.github.com>
2022-06-08 10:50:03 +0200

    update prices plus style

--- javascript.js
+++ javascript.js
@@ -24,3 +24,15 @@ $("#connection").click(function () {
         scrollTop: $("#connect").offset().top
     }, 2000);
 });
+
+const menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+    if (!menuOpen) {
+        menuBtn.classList.add('open');
+        menuOpen = true;
+    } else {
+        menuBtn.classList.remove('open');
+        menuOpen = false;
+    }
+});
