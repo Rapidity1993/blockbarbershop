fd6051918db8e0ae119e51d5b091d9191578e5f2
horvzoltan <zoltanhorvath0721@gmail.com>
2023-06-04 23:19:31 +0200

    refactor

--- javascript.js
+++ javascript.js
@@ -1,38 +1,66 @@
 var slideIndex = 0;
 showSlides();
 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  myFunction();
+};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 7000); // Change image every 2 seconds
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  slides[slideIndex - 1].style.display = "block";
+  setTimeout(showSlides, 7000); // Change image every 2 seconds
 }
 
 $("#workers").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#barbers").offset().top
-    }, 2000);
+  $("html, body").animate(
+    {
+      scrollTop: $("#barbers").offset().top,
+    },
+    2000
+  );
 });
 
 $("#connection").click(function () {
-    $('html, body').animate({
-        scrollTop: $("#connect").offset().top
-    }, 2000);
+  $("html, body").animate(
+    {
+      scrollTop: $("#connect").offset().top,
+    },
+    2000
+  );
 });
 
-const menuBtn = document.querySelector('.menu-btn');
+const menuBtn = document.querySelector(".menu-btn");
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if (!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
+menuBtn.addEventListener("click", () => {
+  if (!menuOpen) {
+    menuBtn.classList.add("open");
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove("open");
+    menuOpen = false;
+  }
 });
